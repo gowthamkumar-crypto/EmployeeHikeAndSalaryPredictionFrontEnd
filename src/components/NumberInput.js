3d92ff51c329e377f689fff611602589217e645f
Gowtham <gowthamkumar8118@gmail.com>
2023-09-12 02:11:07 +0530

    changes in sprint review

---
+++ src/components/NumberInput.js
@@ -0,0 +1,147 @@
+import * as React from 'react';
+import {
+  Unstable_NumberInput as NumberInput,
+  numberInputClasses,
+} from '@mui/base/Unstable_NumberInput';
+import { styled } from '@mui/system';
+
+const CustomNumberInput = React.forwardRef(function CustomNumberInput(props, ref) {
+  return (
+    <NumberInput
+      slots={{
+        root: StyledInputRoot,
+        input: StyledInputElement,
+        incrementButton: StyledButton,
+        decrementButton: StyledButton,
+      }}
+      slotProps={{
+        incrementButton: {
+          children: <span className="arrow">▴</span>,
+        },
+        decrementButton: {
+          children: <span className="arrow">▾</span>,
+        },
+      }}
+      {...props}
+      ref={ref}
+      onChange={(e, value) => {
+        props.onNumberChange(value)
+      }}
+      value={props.value}
+    />
+  );
+});
+
+export default function NumberInputIntroduction({ min, max, onNumberChange, value}) {
+  return (
+    <CustomNumberInput aria-label="Demo number input" placeholder="Type a number…" min={min} max={max} onNumberChange={onNumberChange} value={value}/>
+  );
+}
+
+const blue = {
+  100: '#DAECFF',
+  200: '#b6daff',
+  400: '#3399FF',
+  500: '#007FFF',
+  600: '#0072E5',
+  900: '#003A75',
+};
+
+const grey = {
+  50: '#f6f8fa',
+  100: '#eaeef2',
+  200: '#d0d7de',
+  300: '#afb8c1',
+  400: '#8c959f',
+  500: '#6e7781',
+  600: '#57606a',
+  700: '#424a53',
+  800: '#32383f',
+  900: '#24292f',
+};
+
+const StyledInputRoot = styled('div')(
+  ({ theme }) => `
+  font-family: IBM Plex Sans, sans-serif;
+  font-weight: 400;
+  border-radius: 8px;
+  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+  display: grid;
+  grid-template-columns: 1fr 19px;
+  grid-template-rows: 1fr 1fr;
+  overflow: hidden;
+
+
+  &.${numberInputClasses.focused} {
+    border-color: ${blue[400]};
+  }
+
+  &:hover {
+    border-color: ${blue[400]};
+  }
+
+  // firefox
+  &:focus-visible {
+    outline: 0;
+  }
+`,
+);
+
+const StyledInputElement = styled('input')(
+  ({ theme }) => `
+  font-size: 0.875rem;
+  font-family: inherit;
+  font-weight: 400;
+  line-height: 1.5;
+  grid-column: 1/2;
+  grid-row: 1/3;
+  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  background: inherit;
+  border: none;
+  border-radius: inherit;
+  padding: 8px 12px;
+  outline: 0;
+`,
+);
+
+const StyledButton = styled('button')(
+  ({ theme }) => `
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  appearance: none;
+  padding: 0;
+  width: 19px;
+  height: 19px;
+  font-family: system-ui, sans-serif;
+  font-size: 0.875rem;
+  line-height: 1;
+  box-sizing: border-box;
+  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  border: 0;
+  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+
+  &:hover {
+    background: ${theme.palette.mode === 'dark' ? grey[800] : grey[50]};
+    border-color: ${theme.palette.mode === 'dark' ? grey[600] : grey[300]};
+    cursor: pointer;
+  }
+
+  &.${numberInputClasses.incrementButton} {
+    grid-column: 2/3;
+    grid-row: 1/2;
+  }
+
+  &.${numberInputClasses.decrementButton} {
+    grid-column: 2/3;
+    grid-row: 2/3;
+  }
+
+  & .arrow {
+    transform: translateY(-1px);
+  }
+`,
+);
--- src/components/SprintReview.js
+++ src/components/SprintReview.js
@@ -2,36 +2,124 @@ import { Autocomplete, TextField } from "@mui/material";
 import { useState } from "react";
 import { NavBar } from "./NavBar";
 import axios from "axios";
+import { Form } from 'react-bootstrap';
+import NumberInputIntroduction from "./NumberInput";
 
 const SprintReview = () => {
 
     const [options, setOptions] = useState([]);
+    const [data, setData] = useState({
+      empId: '',
+      projectId: '',
+      workingHours: 0,
+      jobInvolvement: 0,
+      overTime: 0,
+      assignedStoryPoints: 0,
+      spillOverStoryPoints: 0,
+      actualWorkingHours: 0,
+      clientRecognition: '',
+      companyValues: '',
+    });
 
     const onInputChange = ( event , value) => {;
      if(value.length > 2){
         axios.post(`http://127.0.0.1:5000/sprint/review/search/${value}`).then((res)=>{
         if(res.data.length > 0){
-          let modData = [];
-          res.data.map((item,index)=>{
-            modData.push({label:item,id:index})
-          });
+          let modData = res.data.map((item,index)=>({
+            label:item,id:index
+          }));
           setOptions([...modData])
         }   
         });
      }
     }
 
+    const handleValue = (e) => {
+      e.preventDefault();
+    setData({
+      ...data, [e.target.name]: e.target.value
+    });
+    }
+
+    const { empId, projectId, workingHours, jobInvolvement, overTime, assignedStoryPoints, spillOverStoryPoints, actualWorkingHours, clientRecognition, companyValues } = data;
+
     return (
         <>
         <NavBar></NavBar>
+        <div class="row" style={{marginTop: '10%', marginLeft: '5%' }}>
+          <div class="col-4 customPadding" style={{marginTop: '1.5rem'}}>
         <Autocomplete
         options={options}
         onInputChange={onInputChange}
-        style={{ width: 300, background: 'white', top: '25rem', left: '25rem', position: 'relative', borderRadius: '10px'  }}
+        style={{ width: 300, background: 'white', position: 'relative', borderRadius: '10px'  }}
         renderInput={(params) => (
           <TextField {...params} label="Enter employee name" variant="outlined" />
         )}
       />
+      </div>
+      <div class="col-3 customPadding">
+        <label for="empId">Emp Id</label>
+        <input type="text" class="form-control" name="empId" disabled value={empId}/>
+      </div>
+      <div class="col-3 customPadding">
+        <label for="projectId">Project Id</label>
+        <input type="text" class="form-control" name="projectId" disabled value={projectId}/>
+      </div>
+      </div>
+      <div class="row" style={{marginTop: '20px', marginLeft: '5%'}}>
+      <div class="form-group col-3 customPadding">
+          <Form.Label>Sprint Start Date</Form.Label>
+          <Form.Control type="date" name="startDate" placeholder="Sprint Start Date" onChange={e => handleValue(e)}/>
+        </div>
+        <div class="form-group col-3 customPadding">
+          <Form.Label>Sprint End Date</Form.Label>
+          <Form.Control type="date" name="endDate" placeholder="Sprint End Date" onChange={e => handleValue(e)}/>
+        </div>
+        <div class="form-group col-3 customPadding" style={{marginTop: '1.5rem'}}>
+        <NumberInputIntroduction min={0} max={80} onNumberChange={(value) => {
+          setData({...data, workingHours: value })}} value={workingHours}/>
+        </div>
+      </div>
+      <div class="row" style={{marginTop: '20px', marginLeft: '5%'}}>
+      <div class="form-group col-3 customPadding" style={{marginTop: '1.5rem'}}>
+        <NumberInputIntroduction min={0} max={4} onNumberChange={(value) => {
+          setData({...data, jobInvolvement: value })}} value={jobInvolvement}/>
+        </div>
+        <div class="form-group col-3 customPadding" style={{marginTop: '1.5rem'}}>
+        <NumberInputIntroduction min={0} max={40} onNumberChange={(value) => {
+          setData({...data, overTime: value })}} value={overTime}/>
+        </div>
+        <div class="form-group col-3 customPadding" style={{marginTop: '1.5rem'}}>
+        <NumberInputIntroduction min={0} max={8} onNumberChange={(value) => {
+          setData({...data, assignedStoryPoints: value })}} value={assignedStoryPoints}/>
+        </div>
+      </div>
+      <div class="row" style={{marginTop: '20px', marginLeft: '5%'}}>
+      <div class="form-group col-3 customPadding" style={{marginTop: '1.5rem'}}>
+        <NumberInputIntroduction min={0} max={8} onNumberChange={(value) => {
+          setData({...data, spillOverStoryPoints: value })}} value={spillOverStoryPoints}/>
+        </div>
+        <div class="form-group col-3 customPadding" style={{marginTop: '1.5rem'}}>
+        <NumberInputIntroduction min={0} max={80} onNumberChange={(value) => {
+          setData({...data, actualWorkingHours: value })}} value={actualWorkingHours}/>
+        </div>
+        <div class="form-group col-3 customPadding">
+        <select name="clientRecognition"  onChange={e => handleValue(e)} value={clientRecognition} style={{color:'black', width:'100%', height:'80%'}}>
+                                <option>Client Recognition</option>
+                                <option value="yes">yes</option>
+                                <option value="no">no</option>
+        </select>
+        </div>  
+      </div>
+      <div class="row" style={{marginTop: '20px', marginLeft: '5%'}}>
+      <div class="form-group col-3 customPadding">
+        <select name="companyValues"  onChange={e => handleValue(e)} value={companyValues} style={{color:'black', width:'100%', height:'100%'}}>
+                                <option>Adhere client and company values</option>
+                                <option value="yes">yes</option>
+                                <option value="no">no</option>
+        </select>
+        </div>  
+        </div>
       </>
     )
 
